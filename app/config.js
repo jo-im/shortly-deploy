9397e395e8c7b75f440a38cf4bf1cd90f6a7c381
Hack Reactor Students <accounts@hackreactor.com>
2016-07-07 15:19:07 -0700

    Refactor using mongoose

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongod://localhost:4567');
+mongoose.connect('mongodb://localhost/shortly');
 var db = mongoose.connection;
 db.on('error', function(err) {
   console.log('Error connecting to database');
